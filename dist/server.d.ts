fc9d6a889f494d792d6564dc4c553a620dfeda06
nirajgeorgian <niraj.kishore@stayology.com>
2020-06-03 16:10:27 +0530

    removed build files

--- dist/server.d.ts
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-declare class MailServer {
-    start: (port: number) => void;
-}
-declare const start: (port: number) => void;
-export { start };
-export default MailServer;
--- dist/utils/normalize.d.ts
+++
@@ -1,2 +0,0 @@
-declare const normalizePort: (portValue: string) => number;
-export { normalizePort };
